32707b8abe55fae1ecc8929c900782f34f1aec4d
dmitrii-shergin <dmitriishergin.nsk@gmail.com>
2023-03-30 12:15:51 +0400

    add active buttons

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import { renderCity, renderList } from './src/render.js'
 import { getCurrentCity } from './src/storage.js'
 
 const FORM = document.forms.form
+const BUTTON = document.querySelector('.buttons')
+const BUTTONS = document.querySelectorAll('.buttons__mode')
+const TABS = document.querySelectorAll('.info__list-item')
+
+BUTTON.addEventListener('click', onTabClick)
 
 renderList()
 
@@ -16,3 +21,23 @@ FORM.addEventListener('submit', function () {
     renderCity(event, this.input.value)
     this.input.value = ''
 })
+
+function onTabClick(event) {
+    let currentBtn = event.target
+    let tabId = currentBtn.id
+    let currentTab = document.querySelector(`#${tabId}__weather`);
+
+    if (!currentBtn.classList.contains('active')) {
+        console.log(currentTab)
+        BUTTONS.forEach(btn => {
+            btn.classList.remove('active')
+        })
+        TABS.forEach(tab => {
+            tab.classList.remove('active')
+        })
+
+    }
+
+    currentBtn.classList.add('active')
+    currentTab.classList.add('active')
+}
